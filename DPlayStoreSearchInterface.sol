fc30303af34900412eb42d7b14d92111fa936eac
codingcomposer <43369346+codingcomposer@users.noreply.github.com>
2019-06-19 17:17:06 +0900

    Update DPlayStoreSearchInterface.sol

--- DPlayStoreSearchInterface.sol
+++ DPlayStoreSearchInterface.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.5.9;
 
 interface DPlayStoreSearchInterface {
 	
-	// 태그 정보
 	// Tags info
+	// 태그 정보
 	struct GameTags {
 		string tag1;
 		string tag2;
@@ -11,8 +11,8 @@ interface DPlayStoreSearchInterface {
 		string tag4;
 	}
 	
-	// 언어별로 게임의 태그를 입력합니다.
 	// Sets the tags of a game for each language.
+	// 언어별로 게임의 태그를 입력합니다.
 	function setGameDetails(
 		uint gameId,
 		string calldata language,
@@ -21,23 +21,23 @@ interface DPlayStoreSearchInterface {
 		string calldata tag3,
 		string calldata tag4) external;
 	
-	// 출시된 게임 ID들을 가져옵니다.
 	// Gets the IDs of the released games.
+	// 출시된 게임 ID들을 가져옵니다.
 	function getPublishedGameIds() external view returns (uint[] memory);
 	
-	// 게임 ID들을 최신 순으로 가져옵니다.
 	// Gets the game IDs and sort them in the chronological order.
+	// 게임 ID들을 최신 순으로 가져옵니다.
 	function getGameIdsNewest() external view returns (uint[] memory);
-	
-	// 게임 ID들을 높은 점수 순으로 가져오되, 평가 수로 필터링합니다.
+		
 	// Gets game IDs, exclude the games with the low number of ratings and sort the unexcluded games in the descending rating order.
+	// 게임 ID들을 높은 점수 순으로 가져오되, 평가 수로 필터링합니다.
 	function getGameIdsByRating(uint ratingCount) external view returns (uint[] memory);
 	
-	// 태그에 해당하는 게임 ID들을 최신 순으로 가져옵니다.
 	// Gets Game IDs based on the tags and sort them in the chronological order.
+	// 태그에 해당하는 게임 ID들을 최신 순으로 가져옵니다.
 	function getGameIdsByTagNewest(string calldata language, string calldata tag) external view returns (uint[] memory);
 	
-	// 태그에 해당하는 게임 ID들을 높은 점수 순으로 가져오되, 평가 수로 필터링합니다.
 	// Get game IDs based on the tags, exclude the games with the low number of ratings and sort the unexcluded games in the descending rating order.
+	// 태그에 해당하는 게임 ID들을 높은 점수 순으로 가져오되, 평가 수로 필터링합니다.
 	function getGameIdsByTagAndRating(string calldata language, string calldata tag, uint ratingCount) external view returns (uint[] memory);
 }
